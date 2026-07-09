ea8ac85849428277022faab125baf0f1a239ae40
stwwi <123154518+stwwi@users.noreply.github.com>
2026-07-09 14:31:45 +0800

    Update WifiConfig.h

--- User/WifiConfig.h
+++ User/WifiConfig.h
@@ -4,8 +4,8 @@
 #define WIFI_SSID      "账号"
 #define WIFI_PASSWORD  "密码"
 
-#define SERVER_IP      "192.168.2.102"
-#define SERVER_PORT    8080
+#define SERVER_IP      "自家ip"
+#define SERVER_PORT    端口号
 
 #define ESP8266_ENABLE 1
 
